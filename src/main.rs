9b4c73b7d03909dee05e09be5b674a0e6fcadb2e
kaki <kkirvela@gmail.com>
2023-10-12 16:49:08 +0300

    neovim + rope joint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use bevy_rapier2d::prelude::*;
-use noise::permutationtable::PermutationTable;
-use noise::core::open_simplex::*;
-use rand::Rng;
+use bevy::{prelude::*, window::PresentMode};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy::{
-    
-    prelude::*,
-    window::PresentMode,
-};
+use bevy_rapier2d::prelude::*;
+use noise::core::open_simplex::*;
+use noise::permutationtable::PermutationTable;
+use rand::Rng;
 
 mod raycast;
 use raycast::RaycastPlugin;
@@ -18,127 +14,74 @@ use mousezoom::MouseZoomPlugin;
 mod mouseworldpos;
 use mouseworldpos::*;
 
-
-
-
 #[derive(Resource)]
 struct VertsTest {
-    verts: Vec<Vec2>
+    verts: Vec<Vec2>,
 }
 
 #[derive(Resource)]
 struct CubeQuery {
-    cube: Transform
+    cube: Transform,
 }
-
-
 
 #[derive(Resource)]
 struct HasherData {
-    hasher: PermutationTable
+    hasher: PermutationTable,
 }
 
 fn main() {
-    
-
-
-
     App::new()
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "cave-runner".into(),
+                resolution: (1000., 500.).into(),
+                present_mode: PresentMode::AutoVsync,
+                //vsync still on?
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
 
-        .add_plugins(
-            (DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "cave-runner".into(),
-                    resolution: (1000., 500.).into(),
-                    present_mode: PresentMode::AutoVsync,
-                    //vsync still on?
-                    // Tells wasm to resize the window according to the available canvas
-                    fit_canvas_to_parent: true,
-                    // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-                    prevent_default_event_handling: false,
-                    
-                    // This will spawn an invisible window
-                    // The window will be made visible in the make_visible() system after 3 frames.
-                    // This is useful when you want to avoid the white window that shows up before the GPU is ready to render the app.
-                    transparent: false,
-                    
-                    ..default()
-                }),
+                // This will spawn an invisible window The window will be made visible in the make_visible() system after 3 frames. This is useful when you want to avoid the white window that shows up before the GPU is ready to render the app. transparent: false,
                 ..default()
             }),
-            
+            ..default()
+        }),))
+        .add_systems(Startup, setup_physics)
+        .add_plugins((
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
+            RaycastPlugin,
+            MouseZoomPlugin,
+            MouseWorldPos,
         ))
-        .add_systems(Startup,  setup_physics)
-        .add_plugins((RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0), RaycastPlugin, MouseZoomPlugin, MouseWorldPos))
         .add_systems(Update, move_cube)
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(WorldInspectorPlugin::new())
         .insert_resource(RapierContext::default())
-        
         .run();
-
-
-
-    
-
-    
-
-
-    
-
-    
-     
-    
-    
 }
-
-
 
 fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut rng = rand::thread_rng();
-    
-    
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
-            ..default()
-        },
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
-        texture: asset_server.load("square.png"),
-        ..default()
-    }).insert(PlayerMarker);
-        
 
-    
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+            texture: asset_server.load("square.png"),
+            ..default()
+        })
+        .insert(PlayerMarker);
+
     let hasher = PermutationTable::new(rng.gen_range(0..9999));
 
-    
-    
-    
-    commands.insert_resource(HasherData {
-        hasher: hasher
-    });
+    commands.insert_resource(HasherData { hasher: hasher });
 
-    commands.insert_resource(VertsTest {
-        verts: vec![]
-    });
-    
-      
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
+    commands.insert_resource(VertsTest { verts: vec![] });
 
     commands.spawn((Camera2dBundle::default(), MainCamera));
     /* Create the ground. */
@@ -146,67 +89,73 @@ fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
         .spawn(RigidBody::Fixed)
         .insert(Collider::cuboid(500.0, 50.0))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)))
-        
         .id();
 
-    let joint = RevoluteJointBuilder::new()
-    .local_anchor1(Vec2::new(-500.0, 500.0))
-    .local_anchor2(Vec2::new(0.0, 0.0));
+    let joint = RopeJointBuilder::new()
+        .local_anchor1(Vec2::new(0.0, 0.0))
+        .limits([0.5, 1000.0])
+        .local_anchor2(Vec2::new(0.0, 0.0));
     println!("ground id at start {}", ground.index());
-    
 
     /* Create the bouncing ball. */
     let ball: Entity = commands
         .spawn(RigidBody::Dynamic)
         .insert(Collider::ball(50.0))
         .insert(Restitution::coefficient(0.7))
-        
         .insert(GravityScale(10.0))
-        .insert(TransformBundle::from(Transform::from_xyz(100.0, 400.0, 0.0)))
+        .insert(TransformBundle::from(Transform::from_xyz(
+            600.0, 400.0, 0.0,
+        )))
         .id();
 
-    commands.entity(ground).insert(ImpulseJoint::new(ball, joint));
-    
-    
-    
-        
+    commands
+        .entity(ground)
+        .insert(ImpulseJoint::new(ball, joint.build()));
 }
 
 #[derive(Component)]
 struct PlayerMarker;
 
-
-
-
 fn move_cube(
-
-
     mut cubes: Query<&mut Transform, With<PlayerMarker>>,
     mut commands: Commands,
     hasher: Res<HasherData>,
     time: Res<Time>,
     mut gizmos: Gizmos,
-    mut verts: ResMut<VertsTest>
+    mut verts: ResMut<VertsTest>,
 ) {
     commands.insert_resource(CubeQuery {
-        cube: *cubes.single()
+        cube: *cubes.single(),
     });
     for mut cube in cubes.iter_mut() {
-        
-        
         let translation = cube.translation.clone();
 
         let movement_direction = cube.rotation * Vec3::X;
 
         cube.translation += movement_direction * 200.0 * time.delta_seconds();
-        
-        cube.rotate_z(open_simplex_2d::<PermutationTable>([translation.x as f64 / 300.0_f64, translation.y as f64 / 300.0_f64], &hasher.hasher) as f32 * time.delta_seconds() * 5.0);
 
-        gizmos.line(cube.translation, movement_direction * 100.0 + cube.translation, Color::LIME_GREEN);
-        
-        let point_up: Vec2 = Vec2::new(-movement_direction.y, movement_direction.x) * 50.0 + Vec2::new(cube.translation.x, cube.translation.y);
-        let point_down: Vec2 = Vec2::new(movement_direction.y, -movement_direction.x) * 50.0 + Vec2::new(cube.translation.x, cube.translation.y);
-        
+        cube.rotate_z(
+            open_simplex_2d::<PermutationTable>(
+                [
+                    translation.x as f64 / 300.0_f64,
+                    translation.y as f64 / 300.0_f64,
+                ],
+                &hasher.hasher,
+            ) as f32
+                * time.delta_seconds()
+                * 5.0,
+        );
+
+        gizmos.line(
+            cube.translation,
+            movement_direction * 100.0 + cube.translation,
+            Color::LIME_GREEN,
+        );
+
+        let point_up: Vec2 = Vec2::new(-movement_direction.y, movement_direction.x) * 50.0
+            + Vec2::new(cube.translation.x, cube.translation.y);
+        let point_down: Vec2 = Vec2::new(movement_direction.y, -movement_direction.x) * 50.0
+            + Vec2::new(cube.translation.x, cube.translation.y);
 
         verts.verts.push(point_up);
         verts.verts.push(point_down);
@@ -214,24 +163,17 @@ fn move_cube(
         //BAD!! dependent on framerate
         if verts.verts.len() > 1000 {
             for i in 0..verts.verts.len() {
-            
-
-                //remove the start of the cave, so that 
+                //remove the start of the cave, so that
                 if i < verts.verts.len() - 1000 {
                     verts.verts.remove(i);
-                    
                 }
             }
         }
-        
-        
 
         for i in verts.verts.iter() {
             gizmos.line_2d(Vec2::new(i.x - 0.5, i.y - 0.5), *i, Color::LIME_GREEN);
-            
         }
 
         //simple verts working!
     }
-
 }
--- src/raycast.rs
+++ src/raycast.rs
@@ -1,11 +1,9 @@
-use bevy::prelude::*;
-use bevy::transform::commands;
-use crate::{PlayerMarker, CubeQuery};
-use bevy_rapier2d::prelude::*;
 use crate::mouseworldpos::MyWorldCoords;
+use crate::CubeQuery;
 use crate::MainCamera;
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 pub struct RaycastPlugin;
-
 
 //TODO: Be able to cast ray towards mouse
 impl Plugin for RaycastPlugin {
@@ -24,53 +22,40 @@ fn cast_ray(
     mut q_camera: Query<&mut Transform, With<MainCamera>>,
     mut gizmos: Gizmos,
 
-    cube: Option<Res<CubeQuery>>,
-    
-
-    mut commands: Commands
-
+    mut cube: Option<ResMut<CubeQuery>>,
+    //mut commands: Commands
 ) {
-    
-    
-    
-        
-    if let Some(cube) = cube {
-        q_camera.single_mut().translation = cube.cube.translation;
-    }
-    
     //somehow raycast doesn't work perfectly after changing camera position to follow player...
-    let ray_pos = Vec2::new(q_camera.single().translation.x, q_camera.single().translation.y);
+    let ray_pos = Vec2::new(
+        q_camera.single().translation.x,
+        q_camera.single().translation.y,
+    );
     let ray_dir = mousepos.0;
     gizmos.line_2d(ray_pos, ray_dir, Color::LIME_GREEN);
     gizmos.circle_2d(mousepos.0, 5.0, Color::RED);
 
-
     if buttons.just_pressed(MouseButton::Left) {
-        
-        
-        
-        
+        if let Some(mut cubelol) = cube {
+            cubelol.cube.translation.x = mousepos.0.x;
+            cubelol.cube.translation.y = mousepos.0.y;
+            //NOT CHANGING! because we are caching cube in cubelol, so modifying cubelol won't
+            //change cube
+            println!("HELLO!!");
+        }
+
         let max_toi = 4_000_000.0;
-        let solid = false;
+        let solid = true;
         let filter = QueryFilter::default();
-        
 
-        
-
-        if let Some((entity, _toi)) = rapier_context.cast_ray(ray_pos, ray_dir, max_toi, solid, filter) {
+        if let Some((entity, _toi)) =
+            rapier_context.cast_ray(ray_pos, ray_dir, max_toi, solid, filter)
+        {
             // The first collider hit has the entity `entity` and it hit after
             // the ray travelled a distance (vector) equal to `ray_dir * toi`.
             //let hit_point = ray_pos + ray_dir * toi;
             //println!("Entity {:?} hit at point {}", entity, hit_point);
-            
-            
-
 
             println!("raycast id {}", entity.index())
         }
-
-
-    
-
     }
 }
